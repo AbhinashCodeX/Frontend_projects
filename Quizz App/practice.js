ae94bd5a3f65b73ea95f6d2db10770591c55e390
Abhinash Panigrahi <abhinashpanigrahi9@gmail.com>
2024-10-08 07:54:34 +0530

    First Project

---
+++ Quizz App/practice.js
@@ -0,0 +1,39 @@
+const quiz=[
+    {
+        question:"What is the sixe of float and double in java?",
+        a:"32 and 64",
+        b:"32 and 32",
+        c:"64 and 64",
+        d:"64 and 32",
+        correct:"a",
+    },
+    {
+        question:"Automatic type conversion is possible in which of the possible cases? ",
+        a:"Byte to int",
+        b:"Int to Long",
+        c:"Long to int",
+        d:"Short to int",
+        correct:"b",
+
+    },
+    {
+        question:"When an array is passed to a method, what does the method receive?",
+        a:"THe reference of the array",
+        b:"A copy of tthe array",
+        c:"Length of the array",
+        d:"Copy of first Element",
+        correct:"a",
+
+
+
+    },
+    {
+        question:"When is the object created with new keyword?",
+        a:"At run Time",
+        b:"At compile Time",
+        c:"Depends on the code",
+        d:"None",
+        correct:"a",
+    },
+
+];
